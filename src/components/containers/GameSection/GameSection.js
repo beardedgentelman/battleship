7a2bf97a32dfb72cdab26192ae36c8d41056dba7
Andrii <girskiy.andrey@gmail.com>
2023-05-10 09:27:50 +0300

    changin styles of game section. adding separate grid to squares

--- src/components/containers/GameSection/GameSection.js
+++ src/components/containers/GameSection/GameSection.js
@@ -29,29 +29,33 @@ const GameSection = ({ id }) => {
   const squares = generateSquares(rowKeys, colKeys, playerName, squaresRef)
 
   return (
-    <section ref={sectionRef} className='w-fit h-[90vh]' id={id}>
+    <section ref={sectionRef} className='w-fit h-fit' id={id}>
       <h2 className='text-center text-2xl font-bold'>{playerName}</h2>
-      <div className='grid grid-cols-11 place-items-center w-fit h-fit'>
-        <div></div>
-        {colKeys.map((colKey, index) => (
-          <div
-            key={colKey}
-            style={{ gridArea: `${index + 2} / 1 / ${index + 2} / 2` }}
-            className='flex items-center justify-center w-10 h-10'
-          >
-            <span>{colKey}</span>
-          </div>
-        ))}
-        {rowKeys.map((rowKey, index) => (
-          <div
-            key={rowKey}
-            style={{ gridArea: `1 / ${index + 2} / 2 / ${index + 2}` }}
-            className='flex items-center justify-center w-10 h-10'
-          >
-            <span>{rowKey}</span>
-          </div>
-        ))}
-        {squares}
+      <div className='grid w-full' style={{ gridTemplateColumns: 'auto 1fr' }}>
+        <div className='w-fit h-fit'></div>
+        <div className='flex flex-row w-fit h-fit'>
+          {rowKeys.map((rowKey, index) => (
+            <div
+              key={rowKey}
+              style={{ gridArea: `1 / ${index + 2} / 2 / ${index + 2}` }}
+              className='flex items-center justify-center w-10 h-10'
+            >
+              <span>{rowKey}</span>
+            </div>
+          ))}
+        </div>
+        <div className='flex flex-col w-fit h-fit'>
+          {colKeys.map((colKey, index) => (
+            <div
+              key={colKey}
+              style={{ gridArea: `${index + 2} / 1 / ${index + 2} / 2` }}
+              className='flex items-center justify-center w-10 h-10'
+            >
+              <span>{colKey}</span>
+            </div>
+          ))}
+        </div>
+        <div className='grid grid-cols-10 w-[400px] h-fit'>{squares}</div>
       </div>
     </section>
   )
